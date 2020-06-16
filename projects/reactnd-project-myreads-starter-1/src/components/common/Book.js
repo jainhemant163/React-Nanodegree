5ccb08567151236a177c193c38ec5a13a26b41a7
Hemant Jain <jainhemant163@gmail.com>
2020-06-16 17:04:14 -0400

    Reviewer Changes

--- projects/reactnd-project-myreads-starter-1/src/components/common/Book.js
+++ projects/reactnd-project-myreads-starter-1/src/components/common/Book.js
@@ -6,23 +6,33 @@ class Book extends React.Component {
         this.props.changeBookShelf(this.props.book, event.target.value)
 
     render() {
+        const book = this.props.book;
         return (
+            <li key={book.id}>
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundSize: '100% 100%', backgroundImage: 'url(' + this.props.book.imageLinks.smallThumbnail + ')' }}></div>
+                    <div className="book-cover"
+                         style={{
+                             width: 128,
+                             height: 193,
+                             backgroundImage: `url(${book.imageLinks === undefined ? null : book.imageLinks.thumbnail})`
+                         }}>
+                    </div>
                     <div className="book-shelf-changer">
-                    <select onChange={this.onShelfChange} value={this.props.book.shelf}>
-                        <option disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                    </select>
+                        <select value={book.shelf === undefined ? 'none' : book.shelf}
+                                onChange={(event) => this.updateBook(book, event.target.value, this.props.parent, this.props.history)}>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
                     </div>
                 </div>
-                <div className="book-title">{this.props.book.title}</div>
-                <div className="book-authors">{this.props.book.authors}</div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors}</div>
             </div>
+        </li>
         )
     }
 }
